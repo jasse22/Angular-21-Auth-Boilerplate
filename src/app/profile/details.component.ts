10c36dc77564d62effff5a60e6c5b210eef7e6e7
Jessa Mae Jean Dinopol <jessadinopol2005@gmail.com>
2026-05-23 18:12:10 +0800

    the fist commit

---
+++ src/app/profile/details.component.ts
@@ -0,0 +1,13 @@
+﻿import { Component } from '@angular/core';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+    templateUrl: './details.component.html'
+})
+export class DetailsComponent {
+    constructor(private accountService: AccountService) {}
+
+    get account() {
+        return this.accountService.accountValue;
+    }
+}
---
+++ src/app/profile/profile.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ProfileRoutingModule } from './profile-routing.module';  // ← Note: ./ not ../
+import { LayoutComponent } from './layout.component';
+import { DetailsComponent } from './details.component';
+import { UpdateComponent } from './update.component';
+
+@NgModule({
+    declarations: [
+        LayoutComponent,
+        DetailsComponent,
+        UpdateComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterModule,
+        ProfileRoutingModule
+    ]
+})
+export class ProfileModule { }
