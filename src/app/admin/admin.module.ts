3b8b9c91a05adfbcd6c1af1e97a7c168a9de21d2
Jessa Mae Jean Dinopol <jessadinopol2005@gmail.com>
2026-05-23 18:13:11 +0800

    fist

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,24 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AdminRoutingModule } from './admin-routing.module';
+import { SubNavComponent } from './subnav.component';
+import { LayoutComponent } from './layout.component';
+import { OverviewComponent } from './overview.component';
+
+@NgModule({
+    declarations: [
+        SubNavComponent,
+        LayoutComponent,
+        OverviewComponent
+    ],
+    imports: [
+        CommonModule,
+        ReactiveFormsModule,
+        RouterModule,
+        AdminRoutingModule
+    ]
+})
+export class AdminModule { }
